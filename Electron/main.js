f7a82a20004cef4ed4dabc555cf028dfeab1ea67
Inh3ritance <herreralaces@gmail.com>
2021-09-20 19:40:53 -0700

    electron works well

---
+++ Electron/main.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');   
+const path = require('path');
+ 
+let mainWindow;
+ 
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width:800,
+        height:600,
+        show: false,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule:true,
+        },
+        title: "zero-day",
+    });
+    const startURL = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`;
+
+    mainWindow.loadURL(startURL);
+    mainWindow.removeMenu();
+
+    mainWindow.once('ready-to-show', () => mainWindow.show());
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin')
+        app.quit()
+});
+
+app.on('activate', () => {
+    if(mainWindow === null)
+        createWindow()
+});
