6fa69aa1c77946971268fd4f8223ceb3f907ab78
Inh3ritance <herreralaces@gmail.com>
2022-01-21 13:37:43 -0800

    added nodemon

--- Electron/main.js
+++ Electron/main.js
@@ -1,7 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const isDev = require('electron-is-dev');   
 const path = require('path');
-//require('../functions/app');
  
 let mainWindow;
  
